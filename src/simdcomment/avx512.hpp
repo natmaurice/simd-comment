b18bb2809636a6fc17df9082fe1c807286ac60a9
Nathan Maurice <nathan.maurice@protonmail.com>
2024-12-28 18:02:48 +0100

    Use C++14 binary literals instead of C++20 bit_cast

--- src/simdcomment/avx512.hpp
+++ src/simdcomment/avx512.hpp
@@ -4,6 +4,8 @@
 #include <cstdint>
 #include <cstddef>
 
+
+
 void simdc_remove_comments_avx512_vbmi2(const char* input, size_t len, char* output, size_t& olen);
 
 #endif // SIMDCOMMENT_AVX512_HPP_
